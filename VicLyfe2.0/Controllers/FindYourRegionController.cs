0a58917ef84d2f6b3e677859be44012005d277c9
Shuo Wang <swan0017@student.monash.edu>
2019-04-18 18:13:30 +1000

    Add I2 1st page

---
+++ VicLyfe2.0/Controllers/FindYourRegionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VicLyfe2._0.Controllers
+{
+    public class FindYourRegionController : Controller
+    {
+        // GET: FindYourRegion
+        public ActionResult FYR()
+        {
+            return View();
+        }
+    }
+}
